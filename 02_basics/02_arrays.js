5d45c18018b807e01a6bad7101bd9d6567b1c867
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-02-13 05:43:11 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -3,14 +3,12 @@ const dc_heroes = ["superman", "flash", "batman"];
 // marvel_heroes.push(dc_heroes)
 // console.log(marvel_heroes)
 
-
 // console.log(marvel_heroes.concat(dc_heroes));
 // const all_heroes = marvel_heroes.concat(dc_heroes);
 // console.log(all_heroes);
 
 const all_new_heroes = [...marvel_heroes, ...dc_heroes] // spread operator
 console.log(all_new_heroes);
-
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
@@ -19,7 +17,6 @@ console.log(real_another_array);
 
 console.log(Array.isArray("Suraj"));
 console.log(Array.from("Suraj")); // converts string to array
-
 
 let score1 = 100;
 let score2 = 200;
