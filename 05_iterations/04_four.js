ead02bace2486bcaee04f76865f6ba67dfb2d8ba
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-02-20 15:52:30 +0000

    for-in/array

--- 05_iterations/04_four.js
+++ 05_iterations/04_four.js
@@ -11,9 +11,9 @@
 //     console.log(myObject[key]); // it will print all value in myObject
 // }
 
-const programming = ["js", "rb", "py", "java", "cpp"]
+// const programming = ["js", "rb", "py", "java", "cpp"]
 
-for (const key in programming) {
-//    console.log(key);
-    console.log(programming[key]);
-}
+// for (const key in programming) {
+// //    console.log(key);
+//     console.log(programming[key]);
+// }
