6ba2f28aac30779f43d4f7771353b07a419b88ca
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-02-23 06:10:54 +0000

    for-each

--- 05_iterations/05_five.js
+++ 05_iterations/05_five.js
@@ -1,19 +1,46 @@
-// for in loop
-// const myObject = {
-//     js: 'javascript',
-//     cpp: 'C++',
-//     rb: 'ruby',
-//     swift: 'swift by apple'
+//forEach
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+//Function without name
+// coding.forEach(function (element) {
+//     console.log(element);
+// });
+
+//Arrow Function
+// coding.forEach(element => {
+//     console.log(element);
+// });
+
+// Normal function
+// function printMe(coding){
+//   console.log(coding);  
 // }
+// coding.forEach(printMe)
 
-// for (const key in myObject) {
-//     console.log(key); // it will print all keys in myObject
-//     console.log(myObject[key]); // it will print all value in myObject
-// }
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// });
 
-const programming = ["js", "rb", "py", "java", "cpp"]
+const myCoding = [
+    {
+        languageName:"javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },]
+    
+//Call back function
+myCoding.forEach((item)=>{
+console.log(item.languageFileName);
+})
 
-for (const key in programming) {
-//    console.log(key);
-    console.log(programming[key]);
-}
+//Function without name
+myCoding.forEach(function(item){
+    console.log(item.languageFileName);
+    })
