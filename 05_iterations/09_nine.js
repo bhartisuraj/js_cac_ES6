95bce79a251eae68613f80f30626eaa161e30362
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-02-23 06:16:31 +0000

    reduce_method

---
+++ 05_iterations/09_nine.js
@@ -0,0 +1,33 @@
+const myNums = [1, 2, 3]
+// const myTotal = myNums.reduce(function(acc, curVal){
+//     return acc + curVal;
+// },0)/*Here 0 is starting value/initial value*/
+// console.log(myTotal);
+
+// const myTotal = myNums.reduce((acc, curVal)=>{
+//     console.log(`acc: ${acc} and curVal: ${curVal}`);
+//     return acc + curVal
+// },5)/*Here 5 is starting value/initial value*/
+// console.log(myTotal);
+
+// const shoppingCart = [
+//     {
+//     itemName: "js course",
+//     price:2999
+//     },
+//     {
+//     itemName: "py course",
+//     price:999
+//     },
+//     {
+//     itemName: "mobile dev course",
+//     price:5999
+//     },
+//     {
+//     itemName: "data science course",
+//     price:12999
+//     }
+// ]
+// const priceToPay = shoppingCart.reduce((acc, item)=>
+//     acc + item.price ,0)/*Here 5 is starting value/initial value*/
+// console.log(priceToPay);
