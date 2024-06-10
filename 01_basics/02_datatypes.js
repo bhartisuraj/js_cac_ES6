bc56dfa6667a274be2c09394c9883e5254f075cf
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-06-10 07:38:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // tells JS to treat all JS code as newer version of JS
+
+// alert (3 + 3) // We are using node js, not browser - so we can't run this line of statement
+
+console.log(3 + 
+  3) // We can write like this but, its not a good practice.
+
+console.log("Suraj")
+
+let name = "suraj"
+let age = 18
+let isLoggedIn = false
+
+/*1.Primitive Datatype
+number => 2 to power 53
+string => 'Thing inside quotes' 
+bigint => if number is too long(Specially used in stock market/facebook)
+boolean => true or false
+null => standalone value
+undefined => empty value not zero. Variable Which is not defined yet
+symbol => uniquness.*/
+
+/*2.Non-Primitive Datatype/ The Object Datatype
+Object
+Array
+Function*/
+
+console.log(typeof age); // tell variable's datatype
+console.log(typeof null); // tell variable's datatype null => object
+console.log(typeof undefined); // tell variable's datatype undefined => undefined
