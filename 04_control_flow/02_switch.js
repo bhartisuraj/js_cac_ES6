cbb48f4ed58f8e6086470416d7c474bf183bea22
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-06-18 17:17:46 +0000

    switch statements

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,33 @@
+/* It is case sensitive. When some condition matches any value it prints and if does't match to anycase then goes to next statement/default statement, if we don't put break statement after each condition it will goes to next condition like waterfall */
+
+// switch (key) {
+//   case value:
+// console
+//     break;
+//   default:
+// console
+//     break;
+// }
+
+// const month = 3;
+// switch (month) {
+//   case 1:
+//     console.log(`Month 1: January`);
+//     break;
+//   case 2:
+//     console.log(`Month 2: February`);
+//     break;
+//   case 3:
+//     console.log(`Month 3: March`);
+//     break;
+//   case 4:
+//     console.log(`Month 4: April`);
+//     break;
+//   case 5:
+//     console.log(`Month 5: May`);
+//     break;
+
+//   default:
+//     console.log(`End of choices...`);
+//     break;
+// }
