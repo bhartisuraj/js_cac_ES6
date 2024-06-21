6acaac5f75486141ebbe2f72b7d3ee231db4892d
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-06-21 07:44:27 +0000

    while loop and do-while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,24 @@
+// while loop
+// Same as for loop just the syntax is different.
+
+// let i = 0; // Starting point
+// while (i<=5) { // Condition
+//   console.log(`Value of i is : ${i}`);
+//   i++; // increment
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// let i = 0;
+// while (i<myArray.length) {
+//   console.log(`At index ${i} the element is ` + (myArray[i]));
+//   i = i + 1; // i++
+// }
+
+// do-while loop
+// This loop 1st print the statement then check the condition. Hence, when program doesnt match condition then also it runs atleast one time.
+
+// let score = 6;
+// do {
+//   console.log(`Score is ${score}`);
+//   score++;
+// } while (score <= 5);
