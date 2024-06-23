0b0e91c3e4e0ee0ee2528feb2e5fdc8a3e481834
Surajkumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2024-06-23 08:58:17 +0000

    for-in loop

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,34 @@
+// for in loop
+
+//object
+// const myObject = {
+//   js: 'javascript',
+//   cpp: 'C++',
+//   rb: 'ruby',
+//   swift: 'swift by apple'
+// }
+
+// for (const key in myObject) {
+  // console.log(key); // For keys
+  // console.log(`${myObject[key]}`); // For values
+  // console.log(`${key} :- ${myObject[key]}`); // For both keys and values
+// }
+
+//for in loop on arrays
+// const programming = ["js", "rb", "py", "java", "cpp"] // for in loop provide array's index as output.
+
+// for (const key in programming) { // When you run for-in loop it gives keys, where as for-of loop gives value this is one of major difference.
+//   // console.log(key); // for keys
+//   console.log(programming[key]); // for values
+// }
+
+// for in loop on Map()(map do not run via for-in loop)
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
